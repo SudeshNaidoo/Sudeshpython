5c39a26e609372417a0f32c588dd69c539e63fb4
Rooshen <rooshen1@gmail.com>
2024-05-26 20:18:53 +0200

    Login Works :D

--- MainProg.py
+++ MainProg.py
@@ -1,5 +1,35 @@
 import UserClass
+import os
+
+
 User = UserClass.User_class
-print(User.Add)
+
+#UserClass.initialise()
+
+#UserClass.add('Sudesh','123456')
+
+def MainMenu():
+    os.system('clear' if os.name == 'posix' else 'cls')
+    print('++++++++++++++++++++++++++')
+    print('+Welcome to Music Library+')
+    print('++++++++++++++++++++++++++')
+    print()
+    print()
+    print('1 - View Your Music')
+    print('2 - View All Music')
+    print('3 - Delete Your Music')
+    
+
+#UserClass.viewAll()
+username = input("Please enter your Username : ")
+password = input("Please enter your Password : ")
 
 
+if UserClass.verify(username,password) == "Y":
+    MainMenu()
+else:
+    print("IMPOSTER!!!")
+
+
+    
+    
