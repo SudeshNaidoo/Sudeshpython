d2b11c1754a35c183e92fcad72007f5b6e0ae2c1
Sudesh74 <132696113+Sudesh74@users.noreply.github.com>
2024-06-06 21:11:30 +0200

    update music db -secure

---
+++ MusicClassSecure.py
@@ -0,0 +1,132 @@
+import sqlite3
+import datetime
+
+
+
+
+conn = sqlite3.connect('user.db')
+cur = conn.cursor()
+
+currentDateTime = datetime.datetime.now()
+
+# make the object 
+
+#obj = sqlitewrapper.SqliteCipher (dataBasePath="pysqlitecipher.db" ,checkSameThread=False , password='keypassword123456')
+#colList = [
+	#['music', 'CHAR' ] ,
+#	['whatever' , 'int' ] ,
+#]
+
+## created table hash out after creation
+##obj.createTable('testtable' , colList, makeSecure=True , commit=True)
+
+##def printTableData(dataarray):
+	#print(dataarray[0][0].ljust(4,' ')+dataarray[0][1].ljust(10,' ')+dataarray[0][2].ljust(10,' '))
+	#for v in dataarray[1]:
+	#	print(str(v[0]).ljust(4,' ')+v[1].ljust(10,' ')+str(v[2]).ljust(10,' '))
+
+
+
+	
+#insertList = ['musictest', 89]
+#obj.insertIntoTable('testtable' , insertList , commit = True)
+
+#printTableData(
+#obj.getDataFromTable('testtable' , raiseConversionError = True , omitID = False))
+
+#obj.deleteDataInTable('testtable' , 0 , commit= True , raiseError= True , updateId = True)
+
+#print(
+#obj.getDataFromTable('testtable' , raiseConversionError = True , omitID = False))
+
+##obj.getDataFromTable(tableName , raiseConversionError = True , omitID = False)
+
+
+##obj.updateInTable(tableName , iDValue ,colName , colValue , commit = True , raiseError = True)
+###obj.changePassword(newPass)
+
+# MusicClass.py
+#class Music_class:
+
+   ## def add(self,a,b):
+     #   return a
+    
+#def method():
+   # print("OUTPUT")
+
+
+#def add(USERNAME,MusicName , MusicLyrics, MusicScore, MusicType):
+
+    #insert_query = "INSERT INTO Music (USERNAME,MusicName , MusicLyrics, MusicScore, MusicType, CreationDate, ModDate) VALUES (?, ?, ?, ?, ?, ? ,?)"
+    #conn.execute(insert_query, (USERNAME,MusicName , MusicLyrics, MusicScore, MusicType, currentDateTime, currentDateTime))
+    #conn.commit()
+    #conn.close()
+    #viewAll()
+    
+
+
+#def view(username):
+    #cur.execute('select * from Music where USERNAME = ?',(username,))
+    #rows = cur.fetchall()
+
+    #for row in rows:
+       # print(row)
+
+
+##def verify(username,password):
+  ##  cur.execute('select * from Users where USERNAME = ? and PASSWORD = ?',(username,password))
+  ##  rows = cur.fetchall()
+
+   ### for row in rows:
+    ##    return 'Y'
+    
+  ##  return 'N'
+
+    
+
+
+#def viewAll():
+    #cur.execute('select * from Music')
+    #rows = cur.fetchall()
+    #print('Viewing all Music')
+    #print()
+    #for row in rows:
+       # print(row)
+    #conn.close()
+
+
+def initialise():
+
+    ##insecure table creation 
+    ##conn.execute('''DROP TABLE Music''')
+    ##conn.execute(""" CREATE TABLE Music (MusicID INTEGER PRIMARY KEY,
+##MusicName CHAR(25) NOT NULL,
+##MusicLyrics CHAR (255), MusicScore INTEGER(25), MusicType CHAR(20), USERNAME INTEGER, CreationDate TIMESTAMP, ModDate TIMESTAMP);""");
+
+#Secure Table creation 
+    obj.createTable('Music' , colList, makeSecure=True , commit=True)
+    colList = [
+	['MusicName', 'CHAR' ],
+	['MusicLyrics' , 'CHAR' ],
+    ['MusicScore', 'INT'],
+    ['MusicType', 'CHAR'],
+    ['USERNAME','INT'],
+    ['CreationDate', 'TIMESTAMP'],
+    ['MusicID', 'INT PRIMARY KEY'],
+    ['MOdDate', 'TIMESTAMP']
+
+     ]
+    
+    #Encrypt music table DB
+    obj = sqlitewrapper.SqliteCipher (dataBasePath="MusicDBSecure.db" ,checkSameThread=False , password='keypassword123456')
+    colList = [
+	['MusicName', 'CHAR' ],
+	['MusicLyrics' , 'CHAR' ],
+    ['MusicScore', 'INT'],
+    ['MusicType', 'CHAR'],
+    ['USERNAME','INT'],
+    ['CreationDate', 'TIMESTAMP'],
+    ['MusicID', 'INT PRIMARY KEY'],
+    ['MOdDate', 'TIMESTAMP']
+
+     ]
--- helloworld.py
+++ helloworld.py
@@ -1,16 +1,16 @@
 
 import sqlite3
 from pysqlitecipher import sqlitewrapper
-
-class obj:
-	def _init_(self,tableName ,colList, insertList, iDValue, colName, colValue, newPass ):
-		self.tableName = tableName
-		self.colList = colList
-		self.insertList = insertList
-		self.iDValue = iDValue
-		self.colName = colName
-		self.colValue = colValue
-		self.newPass = newPass
+## this is for testing
+##class obj:
+	###def _init_(self,tableName ,colList, insertList, iDValue, colName, colValue, newPass ):
+	#	self.tableName = tableName
+	##	self.colList = colList
+		#self.insertList = insertList
+	#	self.iDValue = iDValue
+		#self.colName = colName
+		#self.colValue = colValue
+		#self.newPass = newPass
 
 
 # make the object 
@@ -24,111 +24,49 @@ colList = [
 ## created table # out after creation
 ##obj.createTable('testtable' , colList, makeSecure=True , commit=True)
 
+def printTableData(dataarray):
+	print(dataarray[0][0].ljust(4,' ')+dataarray[0][1].ljust(10,' ')+dataarray[0][2].ljust(10,' '))
+	for v in dataarray[1]:
+		print(str(v[0]).ljust(4,' ')+v[1].ljust(10,' ')+str(v[2]).ljust(10,' '))
 
+
+
+	
 insertList = ['musictest', 89]
 obj.insertIntoTable('testtable' , insertList , commit = True)
+
+printTableData(
+obj.getDataFromTable('testtable' , raiseConversionError = True , omitID = False))
+
+obj.deleteDataInTable('testtable' , 0 , commit= True , raiseError= True , updateId = True)
 
 print(
 obj.getDataFromTable('testtable' , raiseConversionError = True , omitID = False))
 
-##obj.deleteDataInTable('testtable' , 0 , commit= True , raiseError= True , updateId = True)
-
-##print(
-##obj.getDataFromTable('testtable' , raiseConversionError = True , omitID = False))
-
-obj.getDataFromTable(tableName , raiseConversionError = True , omitID = False)
+##obj.getDataFromTable(tableName , raiseConversionError = True , omitID = False)
 
 
-obj.updateInTable(tableName , iDValue ,colName , colValue , commit = True , raiseError = True)
-obj.changePassword(newPass)
+##obj.updateInTable(tableName , iDValue ,colName , colValue , commit = True , raiseError = True)
+###obj.changePassword(newPass)
 
 
 
-try:
-
-	# Connect to DB and create a cursor
-	sqliteConnection = sqlite3.connect('sql.db')
-	cursor = sqliteConnection.cursor()
-	print('DB Init')
-
-	# Write a query and execute it with cursor
-	query = 'select sqlite_version();'
-	cursor.execute(query)
+#try:
+#
+#	# Connect to DB and create a cursor
+#	sqliteConnection = sqlite3.connect('sql.db')
+##	print('DB Init')
+#
+#	# Write a query and execute it with cursor
+#	query = 'select sqlite_version();'
+#	cursor.execute(query)
 
 	# Fetch and output result
-	result = cursor.fetchall()
-	print('SQLite Version is {}'.format(result))
+##	result = cursor.fetchall()
+	#print('SQLite Version is {}'.format(result))
 
 	# Close the cursor
-	cursor.close()
+	#cursor.close()
 
-# Handle errors
-except sqlite3.Error as error:
-	print('Error occurred - ', error)
 
-# Close DB Connection irrespective of success
-# or failure
-finally:
-
-	if sqliteConnection:
-		sqliteConnection.close()
-		print('SQLite Connection closed')
-
-import sqlite3
-
-#create connection to DB
-cnt = sqlite3.connect('user.db')
-
-# create User class table - hashed to remove duplication 
-#cnt.execute('''CREATE TABLE Users(
-#USERNAME TEXT,
-#PASSWORD TEXT,
-#USERID INTEGER);''')
-
-#Open file in read mode
-#read image as bianary data in variable
-##fileh = open('/C:\Users\Admin\Documents\GitHub\Sudeshpython\Screenshot1.png','rb')
-##img = fileh.read()
-
-#insert tuples for realtion
-###was not created --need method ?
-##cnt.execute('''verify(UserId)
-
-connection = sqlite3.connect('users.db')
-#add to Users table manually test DB
-#connection.execute('INSERT INTO users.db VALUES(Sudesh, abc123456, 1)')
-#connection.execute('INSERT INTO users.db VALUES (TEST, pwd123, 2)')
-#connection.execute('INSERT INTO users.db VALUES (TESTer, pwd1234, 3)')
-
-print('All data in users table\n')
-
-# create a cousor object for select query
-cursor = connection.execute('SELECT * from users')
-
-#display all data from table
-for row in cursor:
-	print(row)
-
-#create class method
-class User_class:
-	
-	def _init_(Self,Verify, Add, DeleteOwnMusic, View, ModOwnMusic, ModifyGenre ):
-		Self.Verify = Verify
-		Self.Add = Add
-		Self.DeleteOwnMusic = DeleteOwnMusic
-		Self.View = View
-		Self.ModifyOwnMusic = ModOwnMusic
-		Self.ModifyGenre = ModifyGenre
-
-    ##@classmethod
-    ##def Add(self, name, birthYear):
-       ## return cls(name, date.today().year - birthYear)
-
- #create Music_class
-class Music_class:
-	def _init_(self,Play ,Download, Upload, Modify):
-		self.Play = Play
-		self.Download = Download
-		self.Upload = Upload
-		self.Modify = Modify
 
