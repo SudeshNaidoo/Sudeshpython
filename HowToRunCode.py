48fe99a69939e109cd17bf68a4da582da0db56f3
Sudesh74 <132696113+Sudesh74@users.noreply.github.com>
2024-06-09 22:25:52 +0200

    updates

--- HowToRunCode.py
+++ HowToRunCode.py
@@ -1,5 +1,5 @@
 #How to run and test this code
-#First run the Setup.py file
+#First run the Setup.py file. This deletes the previous music.db as it will not run on a new PC
 #Check the Testingcodes.py  and unhash the create userid. this will create userid
 #Now run the MainProg.py which will require the userid and password just created.
 
