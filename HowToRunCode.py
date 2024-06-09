e5cc43144b874539c16491aef7f4a6f943723ded
Sudesh74 <132696113+Sudesh74@users.noreply.github.com>
2024-06-09 22:12:14 +0200

    created howtorun code file

---
+++ HowToRunCode.py
@@ -0,0 +1,5 @@
+#How to run and test this code
+#First run the Setup.py file
+#Check the Testingcodes.py  and unhash the create userid. this will create userid
+#Now run the MainProg.py which will require the userid and password just created.
+
